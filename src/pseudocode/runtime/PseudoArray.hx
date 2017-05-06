6b82eebbb0a6ab68ca90f649da68f4ae1c5fac94
Christoph Otter <christophotter@gmail.com>
2017-05-06 02:13:45 +0200

    Shorten PseudoArray a little

--- src/pseudocode/runtime/PseudoArray.hx
+++ src/pseudocode/runtime/PseudoArray.hx
@@ -4,6 +4,11 @@ import haxe.ds.Vector;
 
 class PseudoArray
 {
+    public var length(get, never) : Int;
+
+    inline function get_length() : Int
+        return last - first + 1;
+
     var values : Vector<Dynamic>;
 
     var first : Int;
@@ -17,25 +22,25 @@ class PseudoArray
         if (last < first)
             throw 'Cannot create array of inverted bounds: [$f, $l]';
 
-        values = new Vector<Dynamic>(last - first + 1);
+        values = new Vector<Dynamic>(length);
     }
 
     public function get(i : Int) : Dynamic
-    {
         return values.get(i - first);
-    }
 
     public function set(i : Int, v : Dynamic) : Dynamic
-    {
-        values.set(i - first, v);
-        return v;
-    }
+        return values.set(i - first, v);
 
 
 }
 
 @:forward
 abstract PArray(PseudoArray) {
+
+    public var length(get, never) : Int;
+
+    inline function get_length() : Int
+        return this.length;
 
     public inline function new(first : Int, last : Int)
     {
