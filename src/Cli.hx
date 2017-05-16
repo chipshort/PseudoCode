ba64ddcff5280877a799ae8e771f2aa2c79b6753
Christoph Otter <christophotter@gmail.com>
2017-05-16 09:00:11 +0200

    Add Interpreter tests

--- src/Cli.hx
+++ src/Cli.hx
@@ -25,18 +25,20 @@ class Cli
             return;
         }
 
-        if (args[0] == "--tests")        
-            runTests("tests");
+        if (args[0] == "--tests") { 
+            runParserTests("tests/parser");
+            runInterpTests("tests/interpreter");
+        }
         else {
             var interpreter = new pseudocode.Interpreter();
             Sys.println("Output: " + interpreter.execute(sys.io.File.getContent(args[0])));
         }
     }
 
-    static function runTests(folder : String) : Void
+    static function runParserTests(folder : String) : Void
     {
         Sys.println('==== Running parser tests ====');
-        var testCases = readTests(folder);
+        var testCases = readParserTests(folder);
         var failCounter = 0;
 
         for (test in testCases) {
@@ -101,90 +103,121 @@ class Cli
             Sys.println('$failCounter of ${testCases.length} tests failed ✗');
         else
             Sys.println('All ${testCases.length} tests succeeded ✓');
-
-        //FIXME: add interpreter tests
-
-        // var interp = new pseudocode.Interpreter();
-        // trace(interp.execute("i <- 3 % 2 + 2; return i;")); // 3
-
-        // var interp = new pseudocode.Interpreter();
-        // trace(interp.execute("A[1..2]; A[1] <- 3; return A[1];")); // 3
-
-        // var interp = new pseudocode.Interpreter();
-        // trace(interp.execute("if w2 then return 1; else return 0; fi")); // 0
-
-        // var interp = new pseudocode.Interpreter();
-        // //linear search
-        // trace(interp.execute("
-        // x <- 6;
-        // n <- 5;
-        // A[1..n];
-        // A[1] <- 1;
-        // A[2] <- 2;
-        // A[3] <- 3;
-        // A[4] <- 4;
-        // A[5] <- 5;
-
-        // i <- 1;
-        // while i ≤ n ∧ A[i] ≠ x do
-        //     i <- i + 1;
-        // od
-        // if i > n then i <- 0; fi
-
-        // return i;
-        // ")); //0
-
-        // //binarysearch
-        // var interp = new pseudocode.Interpreter();
-        // trace(interp.execute("
-        // x <- 1;
-        // n <- 5;
-        // A[1..n];
-        // A[1] <- 1;
-        // A[2] <- 2;
-        // A[3] <- 3;
-        // A[4] <- 4;
-        // A[5] <- 5;
-        
-        // l <- 1; r <- n;
-        // while l ≤ r do
-        //     m <- ⌊(l + r) / 2⌋;
-        //     if A[m] = x then
-        //         return m;
-        //     fi
-        //     if x > A[m] then
-        //         l <- m + 1;
-        //     else
-        //         r <- m + 1;
-        //     fi
-        // od
-        // return 0;
-        // ")); //1
     }
 
-    static function readTests(folder : String) : Array<Test>
+    static function runInterpTests(folder : String) : Void
     {
-        var tests = new Array<Test>();
+        Sys.println('==== Running interpreter tests ====');
+        var testCases = readInterpTests(folder);
+        var failCounter = 0;
+
+        for (test in testCases) {
+            var interp = new pseudocode.Interpreter();
+
+            var input = byte.ByteData.ofString(test.code);
+            var parser = new pseudocode.PseudoParser(input, test.file);
+            var lexer = new pseudocode.PseudoLexer(input, test.file);
+            
+            var expr = try {
+                parser.parseCode();
+            }
+            catch (e : Dynamic) {
+                failCounter++;
+                Sys.println('=== ${test.file} ===');
+
+                Sys.println("--- Code ---");
+                Sys.println(test.code);
+                Sys.println("--- Tokens ---");
+                var token = lexer.token(pseudocode.PseudoLexer.tok);
+                while (token != Eof) {
+                    Sys.println(token);
+                    token = lexer.token(pseudocode.PseudoLexer.tok);
+                }
+                Sys.println("--- could not be parsed: ---");
+                #if neko
+                neko.Lib.rethrow(e);
+                #else
+                throw e;
+                #end
+            }
+            
+            var result = interp.execute(test.code);
+
+            if (result != true) {
+                failCounter++;
+                Sys.println('=== ${test.file} ===');
+
+                Sys.println("--- Code ---");
+                Sys.println(test.code);
+                Sys.println("-- returned --");
+                Sys.println(result);
+                Sys.println("-- raw expressions --");
+                Sys.println(expr);
+
+                Sys.println("--- Tokens ---");
+                var token = lexer.token(pseudocode.PseudoLexer.tok);
+                while (token != Eof) {
+                    Sys.println(token);
+                    token = lexer.token(pseudocode.PseudoLexer.tok);
+                }
+
+                Sys.println('=== End of ${test.file} ===');
+                Sys.println("");
+            }
+        }
+
+        if (failCounter != 0)
+            Sys.println('$failCounter of ${testCases.length} tests failed ✗');
+        else
+            Sys.println('All ${testCases.length} tests succeeded ✓');
+    }
+
+    inline static function readInterpTests(folder : String) : Array<InterpTest>
+    {
+        return parseFilesIn(folder, function (file) {
+            var test = {
+                code: sys.io.File.getContent(file),
+                file: file
+            }
+            return test;
+        });
+    }
+
+    inline static function readParserTests(folder : String) : Array<ParserTest>
+    {
+        return parseFilesIn(folder, function (file) {
+            var content : ParserTest = haxe.Json.parse(sys.io.File.getContent(file));
+            content.file = file;
+            return content;
+        });
+    }
+
+    static function parseFilesIn<T>(folder : String, parse : String->T) : Array<T>
+    {
+        var files = new Array<T>();
 
         for (file in sys.FileSystem.readDirectory(folder)) {
             var fullFile = haxe.io.Path.join([folder, file]);
 
             if (sys.FileSystem.isDirectory(fullFile)) {
-                tests = tests.concat(readTests(fullFile));
+                files = files.concat(parseFilesIn(fullFile, parse));
             }
             else {
-                var content = haxe.Json.parse(sys.io.File.getContent(fullFile));
-                content.file = fullFile;
-                tests.push(content);
+                files.push(parse(fullFile));
             }
         }
 
-        return tests;
+        return files;
     }
 }
 
-typedef Test = {
+typedef ParserTest = {
     code : String,
     expected : String,
+    file : String
+}
+
+typedef InterpTest = {
+    code : String,
     file : String
 }
